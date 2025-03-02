299936587a3c03003bec32313d231f664abd91f9
Pavel Vdovenko <reactivecake@gmail.com>
2025-03-02 04:52:57 +0400

    feat(dummy_server.py): logging

--- dummy_server.py
+++ dummy_server.py
@@ -1,12 +1,22 @@
-from flask import Flask, redirect
+import os
+import logging
+from flask import Flask, redirect, request
 
+# Create Flask app
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+@app.before_request
+def log_request():
+    logger.info(f"Request from {request.remote_addr} to {request.path} with method {request.method}")
 
 @app.route("/")
 def redirect_to_google():
     return redirect("https://www.google.com", code=302)
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 8000))  # Ensure compatibility with Heroku
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
